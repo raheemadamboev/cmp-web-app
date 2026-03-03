09b058765174c6a57c96eb9ae6a4f29ad37bec8b
raheem <raheemideadev@gmail.com>
2026-03-03 16:02:02 +0500

    Implemented the app;

--- composeApp/src/commonMain/kotlin/xyz/teamgravity/cmpwebapp/App.kt
+++ composeApp/src/commonMain/kotlin/xyz/teamgravity/cmpwebapp/App.kt
@@ -1,47 +1,71 @@
 package xyz.teamgravity.cmpwebapp
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import cmpwebapp.composeapp.generated.resources.Res
-import cmpwebapp.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    onCountChange: (count: Int) -> Unit = {}
+) {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+        var count by remember { mutableIntStateOf(0) }
+
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(
+                space = 16.dp,
+                alignment = Alignment.CenterVertically
+            ),
+            modifier = Modifier.fillMaxSize()
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+            Text(
+                text = count.toString(),
+                fontSize = 40.sp
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(
+                    space = 16.dp,
+                    alignment = Alignment.CenterHorizontally
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {
+                        count--
+                        onCountChange(count)
+                    }
                 ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text(
+                        text = "-"
+                    )
+                }
+                Button(
+                    onClick = {
+                        count++
+                        onCountChange(count)
+                    }
+                ) {
+                    Text(
+                        text = "+"
+                    )
                 }
             }
         }
--- composeApp/src/webMain/kotlin/xyz/teamgravity/cmpwebapp/main.kt
+++ composeApp/src/webMain/kotlin/xyz/teamgravity/cmpwebapp/main.kt
@@ -2,10 +2,21 @@ package xyz.teamgravity.cmpwebapp
 
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
+import kotlin.js.js
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport {
-        App()
+        App(
+            onCountChange = { count ->
+                if (count == 10) {
+                    alert()
+                }
+            }
+        )
     }
+}
+
+private fun alert() {
+    js(" { alert(\"Let's gooooo!\") }")
 }
