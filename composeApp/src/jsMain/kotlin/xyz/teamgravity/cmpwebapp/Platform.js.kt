0cd238f7768751609c2c4e25c496d78f70de5cd0
raheem <raheemideadev@gmail.com>
2026-03-03 16:01:27 +0500

    Removed unnecessary files;

--- composeApp/src/jsMain/kotlin/xyz/teamgravity/cmpwebapp/Platform.js.kt
+++
@@ -1,7 +0,0 @@
-package xyz.teamgravity.cmpwebapp
-
-class JsPlatform: Platform {
-    override val name: String = "Web with Kotlin/JS"
-}
-
-actual fun getPlatform(): Platform = JsPlatform()
--- composeApp/src/wasmJsMain/kotlin/xyz/teamgravity/cmpwebapp/Platform.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-package xyz.teamgravity.cmpwebapp
-
-class WasmPlatform: Platform {
-    override val name: String = "Web with Kotlin/Wasm"
-}
-
-actual fun getPlatform(): Platform = WasmPlatform()
